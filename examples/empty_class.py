23f2154e8e758746a87e92be3cdbd3c572a69275
aaa <achapialiou@gmail.com>
2025-06-22 14:36:22 +0300

    Remove methods from EmptyClass

--- examples/empty_class.py
+++ examples/empty_class.py
@@ -2,17 +2,3 @@ class EmptyClass:
     def __init__(self):
         pass
 
-    def method1(self):
-        pass
-
-    def method2(self):
-        pass
-
-    def method3(self):
-        pass
-
-    def method4(self):
-        pass
-
-    def method5(self):
-        pass
